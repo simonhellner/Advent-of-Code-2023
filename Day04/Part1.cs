80b6473f0ddc907c84eb2c10477b16193d668484
simonhellner <85552673+simonhellner@users.noreply.github.com>
2023-12-05 16:57:24 +0100

    Day 04

--- Day04/Part1.cs
+++ Day04/Part1.cs
@@ -4,6 +4,40 @@ public static class Part1
 {
     public static void Part1Main()
     {
+        var input = File.ReadAllLines("input.txt");
 
+        int totalPoints = 0;
+        foreach (var card in input)
+        {
+            var split = card.Split('|');
+
+            var numbers = split[0]
+                .Split(": ")[1]
+                .Split(' ')
+                .Where(x => x != "")
+                .Select(x => int.Parse(x))
+                .ToList();
+
+            var winningNumbers = split[1]
+                .Split(' ')
+                .Where(x => x != "")
+                .Select(x => int.Parse(x))
+                .ToList();
+
+            var correctNumbers = numbers.Where(number => winningNumbers.Contains(number)).ToList();
+            if (correctNumbers.Count < 1)
+                continue;
+
+            if (correctNumbers.Count == 1)
+            {
+                totalPoints += 1;
+            }
+            else
+            {
+                var points = Math.Pow(2, correctNumbers.Count) - Math.Pow(2, correctNumbers.Count - 1);
+                totalPoints += (int)points;
+            }
+        }
+        Console.WriteLine($"Part 1, Total Points =>  {totalPoints}");
     }
 }
--- Day04/Part2.cs
+++ Day04/Part2.cs
@@ -2,8 +2,71 @@
 
 public static class Part2
 {
+    public class Card
+    {
+        public int CopiesOfThisCard { get; set; } = 1;
+
+        public List<int> CorrectNumbers { get; set; }
+    }
+
+
+    static List<Card> GetInputPart2()
+    {
+        var input = File.ReadAllLines("input.txt");
+        List<Card> cards = [];
+        foreach (var card in input)
+        {
+            var split = card.Split('|');
+
+            var numbers = split[0]
+                .Split(": ")[1]
+                .Split(' ')
+                .Where(x => x != "")
+                .Select(x => int.Parse(x))
+                .ToList();
+
+            var winningNumbers = split[1]
+                .Split(' ')
+                .Where(x => x != "")
+                .Select(x => int.Parse(x))
+                .ToList();
+
+            cards.Add(new Card()
+            {
+                CorrectNumbers = numbers.Where(number => winningNumbers.Contains(number)).ToList(),
+            });
+        }
+
+        return cards;
+
+    }
+
+
     public static void Part2Main()
     {
+        List<Card> cards = GetInputPart2();
 
+        for (int i = 0; i < cards.Count; i++)
+        {
+            var card = cards[i];
+
+            for (int copies = 0; copies < card.CopiesOfThisCard; copies++)
+            {
+                int correctNumbers = card.CorrectNumbers.Count;
+
+                if (correctNumbers < 1)
+                    continue;
+
+                var cardsToCopy = i + correctNumbers;
+                for (int j = i + 1; j <= (i + correctNumbers) && j < cards.Count; j++)
+                {
+                    cards[j].CopiesOfThisCard += 1;
+                }
+            }
+        }
+
+        var totalCards = cards.Sum(x => x.CopiesOfThisCard);
+
+        Console.WriteLine($"Part 2, Total Cards =>  {totalCards}");
     }
 }
