f031d1348a836c04e464cd6990ff94de5b9e441b
simonhellner <85552673+simonhellner@users.noreply.github.com>
2023-12-09 09:36:15 +0100

    Day 09

--- Day09/Part1.cs
+++ Day09/Part1.cs
@@ -2,8 +2,40 @@
 
 public static class Part1
 {
+    static int Something(int[] row)
+    {
+        List<int> NextRow = [];
+        for (int i = 0; i < row.Length-1; i++)
+        {
+            var diff = row[i + 1] - row[i];
+            NextRow.Add(diff);
+        }
+
+        int lastNubmerNextRow = 0;
+        if (NextRow.Any(diff => diff != 0))
+        {
+            lastNubmerNextRow = Something(NextRow.ToArray());
+        }
+
+        var lastNumberThisRow = row.Last();
+        var newLastNumber = lastNumberThisRow + lastNubmerNextRow;
+
+        return newLastNumber;
+    }
+
     public static void Part1Main()
     {
+        var input = File.ReadAllLines("input.txt")
+            .Select(line => line.Split(' ').Select(int.Parse).ToArray())
+            .ToArray();
 
+        int sum = 0;
+        foreach (var line in input)
+        {
+            var output =   Something(line);
+            sum += output;
+        }
+
+        Console.WriteLine($"Part 1, result => {sum}");
     }
 }
