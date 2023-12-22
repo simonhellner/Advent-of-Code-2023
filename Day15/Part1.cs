758f4236ef558181b735e997c96fb77042a46bb6
simonhellner <85552673+simonhellner@users.noreply.github.com>
2023-12-22 14:15:06 +0100

    Day 15, Part 1

--- Day15/Part1.cs
+++ Day15/Part1.cs
@@ -2,8 +2,31 @@
 
 public static class Part1
 {
+    static int Hash(string source)
+    {
+        int currentValue = 0;
+        foreach (char c in source)
+        {
+            int ascii = (int)c;
+            currentValue += ascii;
+            currentValue *= 17;
+            currentValue %= 256;
+        }
+
+        return currentValue;
+    }
+
     public static void Part1Main()
     {
+        string input = File.ReadAllText("input.txt");
 
+        int sum = 0;
+        foreach (var step in input.Split(','))
+        {
+            var hash = Hash(step);
+            sum += hash;
+        }
+
+        Console.WriteLine($"Part 1 result, {sum}");
     }
 }
